2dc89eafdf370332a6c519c693d86e3aef05c041
touya-akira <touya.akira@yandex.com>
2013-01-20 23:26:55 +0400

    reactions

--- modules/reactions.py
+++ modules/reactions.py
@@ -1,7 +1,15 @@
+def clients(phenny, input):
+	phenny.say('There are several IRC clients for all operating systems. For Windows, we recommend hexchat (Free X-Chat Successor, http://hexchat.org). For Unix/Linux consider X-Chat (GUI) or irssi (console), both available in your repository. For Mac try XChat Aqua (http://xchataqua.github.com/)')
+clients.rule = r'^clients\?'
+
 def dnsleaks(phenny, input):
         phenny.say('A DNS leak means that you are using a fixed nameserver      (like the one from your provider) which may compromise your anonymity. Read     about it here: http://www.dnsleaktest.com/what-is-a-dns-leak.php')
         phenny.say('You can test this on this website: http://www.dnsleaktest.  com/ - to prevent DNS leaks when using TOR, read the following: https://trac.   torproject.org/projects/tor/wiki/doc/PreventingDnsLeaksInTor')
 dnsleaks.rule = r'dnsleaks?'
+
+def freevpn(phenny, input):
+	phenny.say('Personally I would not trust any free VPN for the simple reason that if a VPN provider has no product for sale, chances are that they are selling YOU.')
+freevpn.rule = r'^freevpn\?'
 
 def hacking(phenny, input):
         phenny.say('How to Become a Hacker: http://www.catb.org/esr/faqs/hacker-howto.html')
@@ -17,7 +25,7 @@ learnPython.rule = r'^python\?'
 learnPython.priority = 'medium'
 
 def topics(phenny, input):
-        phenny.say('Currently I can advise about the following topics: dnsleaks, hacking, i2p, python, vpn and tor.')
+        phenny.say('Currently I can advise about the following topics: clients, freevpn, dnsleaks, hacking, i2p, python, vpn and tor.')
 topics.rule = "^topics\?"
 
 def tor(phenny, input):
@@ -25,6 +33,14 @@ def tor(phenny, input):
 tor.rule = r'^tor\?'
 
 def vpn(phenny, input):
-        phenny.say('Currently I can recommend two privacy vpn providers, '+     input.nick+': Perfect Privacy (http://perfect-privacy.com) and ovpn (http://    ovpn.to), ')
+        phenny.say('Currently I can recommend two privacy vpn providers, '+     input.nick+': Perfect Privacy (http://perfect-privacy.com) and ovpn (http://ovpn.to)')
 vpn.rule = r'^vpn\?'
 vpn.priority = 'medium'
+
+
+#generic stuff
+
+def Logbot(phenny, input):
+	phenny.say('lol logbots <3')
+Logbot.rule = r'.*logbot.*'
+
