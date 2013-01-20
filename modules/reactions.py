4acadd400ffe95e8a2b494870daff95de3aa70c6
touya-akira <touya.akira@yandex.com>
2013-01-20 22:37:17 +0400

    more reactions

--- modules/reactions.py
+++ modules/reactions.py
@@ -1,22 +1,30 @@
-def topics(phenny, input):
-	phenny.say('Currently I can advise about the following topics: dnsleaks, hacking, python, vpn.')
-topics.rule = r'topics?'
+def dnsleaks(phenny, input):
+        phenny.say('A DNS leak means that you are using a fixed nameserver      (like the one from your provider) which may compromise your anonymity. Read     about it here: http://www.dnsleaktest.com/what-is-a-dns-leak.php')
+        phenny.say('You can test this on this website: http://www.dnsleaktest.  com/ - to prevent DNS leaks when using TOR, read the following: https://trac.   torproject.org/projects/tor/wiki/doc/PreventingDnsLeaksInTor')
+dnsleaks.rule = r'dnsleaks?'
 
 def hacking(phenny, input):
-	phenny.say('How to Become a Hacker: http://www.catb.org/esr/faqs/hacker-howto.html')
+        phenny.say('How to Become a Hacker: http://www.catb.org/esr/faqs/hacker-howto.html')
 hacking.rule = r'hacking?'
 
-def learnPython(phenny, input):
-	phenny.say('I recommend: http://learnpythonthehardway.org/')
+def i2p(phenny, input):
+	phenny.say('I2P is an anonymous network, exposing a simple layer that applications can use to anonymously and securely send messages to each other. http://www.i2p2.de/intro.html - http://www.i2p2.de/faq.html')
+i2p.rule = r'i2p?'
+
+def python(phenny, input):
+        phenny.say('I recommend: http://learnpythonthehardway.org/')
 learnPython.rule = r'python?'
 learnPython.priority = 'medium'
 
+def topics(phenny, input):
+        phenny.say('Currently I can advise about the following topics:          dnsleaks, hacking, python, vpn.')
+topics.rule = r'topics?'
+
+def tor(phenny, input):
+	phenny.say('Tor is a network of virtual tunnels that allows people and groups to improve their anonymity and security on the Internet. Read more here: https://www.torproject.org/about/overview.html.en and here https://www.torproject.org/docs/documentation.html.en')
+tor.rule = r'tor?'
+
 def vpn(phenny, input):
-	phenny.say('Currently I can recommend two privacy vpn providers, '+input.nick+': Perfect Privacy (http://perfect-privacy.com) and ovpn (http://ovpn.to), ')
+        phenny.say('Currently I can recommend two privacy vpn providers, '+     input.nick+': Perfect Privacy (http://perfect-privacy.com) and ovpn (http://    ovpn.to), ')
 vpn.rule = r'vpn?'
 vpn.priority = 'medium'
-
-def dnsleaks(phenny, input):
-	phenny.say('A DNS leak means that you are using a fixed nameserver (like the one from your provider) which may compromise your anonymity. Read about it here: http://www.dnsleaktest.com/what-is-a-dns-leak.php')
-	phenny.say('You can test this on this website: http://www.dnsleaktest.com/ - to prevent DNS leaks when using TOR, read the following: https://trac.torproject.org/projects/tor/wiki/doc/PreventingDnsLeaksInTor')
-dnsleaks.rule = r'dnsleaks?'
